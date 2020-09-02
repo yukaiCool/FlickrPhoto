34cddbf02171e28808e47d7e45055604f5938183
李御楷 <monkey61106@gmail.com>
2020-09-02 11:21:31 +0800

    // create app

---
+++ FlickrPhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrPhoto
+//
+//  Created by 李御楷 on 2020/9/2.
+//  Copyright © 2020 李御楷. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
